d66f54e54edc08b1933d3687aedef0ae6ede2ccf
Evan Kivolowitz <ekivolowitz@wisc.edu>
2019-02-14 20:42:33 -0600

    added images and example code

--- JSONPAttack/api.py
+++ JSONPAttack/api.py
@@ -1,9 +1,30 @@
-from flask import Flask, jsonify, request
+from functools import wraps
+from flask import request, current_app, jsonify, app, Flask
 
 app = Flask(__name__)
-@app.route("/", methods=['GET'])
-def get():
-    return jsonify({"blah" : "merp"})
 
-if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000)
+
+def support_jsonp(f):
+    """Wraps output to JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        result = jsonify(f(*args, **kwargs))
+        callback = request.args.get('callback', False)
+        if callback:
+            print(str(result.data))
+            content = callback + '(' + result.data + ')'
+            return current_app.response_class(content,
+                                              mimetype='application/json')
+        else:
+            return result
+    return decorated_function
+
+
+# then in your view
+@app.route('/', methods=['GET'])
+@support_jsonp
+def test():
+    return {'foo': 'bar', "moo" : "mar"}
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port=5000)
